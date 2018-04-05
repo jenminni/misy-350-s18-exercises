ac02e905da8e1149f290fd8aa5f04fdc6f272f6b
Jen Minnich <jminnich@udel.edu>
2018-04-05 10:37:55 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,11 @@
+blist = ["end of the semester", "no class", "beach"];
+console.log(blist.length);
+blist.push("get a beer");
+console.log(blist.length);
+console.log(blist.length);
+console.log(blist);
+
+//for loop
+for (i=0; i<blist.length; i++) {
+  console.log(blist[i]);
+}
