3ddc6cc5ecc5343899d235e7c99e7aacae11df26
Jen Minnich <jminnich@udel.edu>
2018-04-19 10:25:59 -0400

    add exercises

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "hello"
+name = "Jen"
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %d! how are you doing, today" % (greeting, 10)
+#%d stands for data -- placeholder for a number
